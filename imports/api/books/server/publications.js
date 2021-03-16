12d5aeb3a9520ee978a3e728a0efaa705dfd8ccf
vmusulainen <vmusulainen@gmail.com>
2021-03-16 14:11:59 +0300

    support re-fetching data from API on book creation

--- imports/api/books/server/publications.js
+++ imports/api/books/server/publications.js
@@ -1,3 +1,8 @@
+import EventBus from 'js-event-bus';
+
+const eventBus = EventBus();
+
+
 Meteor.publish('books.all', function () {
     const items = {};
     //the first adding all fetched items to local store
@@ -8,23 +13,24 @@ Meteor.publish('books.all', function () {
         .then((json) => {
             json.forEach((each) => {
                 const id = each.id;
-                delete(each.id);
+                delete (each.id);
                 items[id] = each;
                 this.added('books', id, each);
             });
             this.ready();
         });
 
-    Meteor.setInterval(() => {
+
+    const timeInterval = Meteor.setInterval(() => {
         fetch('http://localhost:4000/books')
             .then((res) => {
                 return res.json();
             })
             .then((json) => {
-                console.log('refreshing data');
+                console.log('Timer: refreshing data');
                 json.forEach((each) => {
                     const id = each.id;
-                    delete(each.id);
+                    delete (each.id);
                     if (items[id] == null) {
                         items[id] = each;
                         this.added('books', id, each);
@@ -35,12 +41,82 @@ Meteor.publish('books.all', function () {
                 });
                 this.ready();
             });
-    }, 1000)
+    }, 10000);
+
+    eventBus.on('books.create', () => {
+        console.log('Forced: refreshing data');
+        fetch('http://localhost:4000/books')
+            .then((res) => {
+                return res.json();
+            })
+            .then((json) => {
+                json.forEach((each) => {
+                    const id = each.id;
+                    delete (each.id);
+                    if (items[id] == null) {
+                        items[id] = each;
+                        this.added('books', id, each);
+                    } else {
+                        items[id] = each;
+                        this.changed('books', id, each);
+                    }
+                });
+            });
 
 
 
 
+    });
 
+
+    eventBus.on('books.create', () => {
+        console.log('empty event handler')
+    });
+
+
+    this.onStop(() => {
+        eventBus.die('books.create');
+        Meteor.clearInterval(timeInterval);
+    })
 
 
 })
+
+Meteor.methods({
+    'books.create'(data) {
+        fetch('http://localhost:4000/books', {
+            method: 'post',
+            body: JSON.stringify(data),
+            headers: {'Content-Type': 'application/json'},
+        })
+            .then((res) => {
+                return res.json()
+            })
+            .then((json) => {
+                eventBus.emit('books.create');
+            });
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- imports/ui/pages/books-pages/books-index.js
+++ imports/ui/pages/books-pages/books-index.js
@@ -1,12 +1,26 @@
 import './books-index.html';
 import {Books} from '../../../api/books/books';
 
-Template.Books.onCreated(function(){
+Template.Books.onCreated(function () {
     this.subscribe('books.all');
 })
 
 Template.Books.helpers({
-    books(){
-        return Books.find();
+    books() {
+        return Books.find({}, {sort: {_id: -1}});
+    }
+});
+
+Template.Books.events({
+    'click [data-action=book-create]'() {
+        const author = `Author: ${new Date().valueOf()}`;
+        const title = `Title: ${new Date().valueOf()}`;
+        Meteor.call('books.create', {author, title}, (err, res) => {
+                if (err) {
+                    console.log('err', err);
+                }
+            }
+        )
     }
 })
+
